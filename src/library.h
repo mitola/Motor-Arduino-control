2e2a2f67d38c8fde0ab25effae8f1430bfd0339f
mitola <matjaz.trcek1@gmail.com>
2017-02-01 16:01:11 +0000

    Nicefying DC motor robot and functionalizing it

---
+++ src/library.h
@@ -0,0 +1,8 @@
+void shiftWrite(int output, int high_low);
+void motor_output (int output, int high_low, int speed);
+void motor(int nMotor, int command, int speed);
+void stop();
+void forward(int timeMs);
+void backward(int timeMs);
+void turn(int direction, int timeMs);
+void setupRobot();
